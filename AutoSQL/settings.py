ef06496c47adec11e13e8cf8b6f397931f7cca2c
Sparsh <sparshsahu567@gmail.com>
2025-09-06 21:46:25 +0530

    fix default superuser

--- AutoSQL/settings.py
+++ AutoSQL/settings.py
@@ -13,9 +13,6 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
-
-# Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
